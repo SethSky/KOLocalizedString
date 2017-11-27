2d1be07c1b577a6a334bbfb17c6da83918426476
Oleksandr Khymych <art@khymych.com>
2017-11-27 12:10:31 +0200

    add info.plist add KOLocalizedString.h

---
+++ Source/KOLocalizedString.h
@@ -0,0 +1,15 @@
+//
+//  KOLocalizedString.h
+//  KOLocalizedString
+//
+//  Created by Oleksandr Khymych on 27.11.2017.
+//  Copyright © 2017 Oleksandr Khymych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KOLocalizedString.
+FOUNDATION_EXPORT double KOLocalizedStringVersionNumber;
+
+//! Project version string for KOLocalizedString.
+FOUNDATION_EXPORT const unsigned char KOLocalizedStringVersionString[];
